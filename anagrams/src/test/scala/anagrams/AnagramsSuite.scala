982d01a2dbb2bfe1e712b9c6b96c8f34fbcea7c2
woidveit02 <108467586+woidveit02@users.noreply.github.com>
2024-05-30 01:19:51 +0200

    Remove unused test
    Removed:
    - unused test

--- anagrams/src/test/scala/anagrams/AnagramsSuite.scala
+++ anagrams/src/test/scala/anagrams/AnagramsSuite.scala
@@ -364,9 +364,3 @@ class AnagramsSuite extends munit.FunSuite:
     )
 
     assertEquals(sentenceAnagrams(able, sentence).toSet, anas.toSet)
-
-class HoursTest extends munit.FunSuite:
-  // This final test checks that you filled in the `time spent`
-  // question at the end of the lab.
-  test("After completing the lab, please report how long you spent on it (1pt)"):
-    assert(howManyHoursISpentOnThisLab() > 0.0)
